064f0c09207a11234df54dacb32582d884f51b7b
vgmoose <me@vgmoose.com>
2021-10-23 00:45:46 -0400

    Initial Commit

---
+++ TheNotch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TheNotch
+//
+//  Created by Ricky on 10/23/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TheNotch/TheNotchApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheNotchApp.swift
+//  TheNotch
+//
+//  Created by Ricky on 10/23/21.
+//
+
+import SwiftUI
+
+@main
+struct TheNotchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
